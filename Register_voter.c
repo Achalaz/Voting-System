dbcc904dacd70cc9a6bdc620869fd050b13c01f2
Achala <achalaeshan20@gmail.com>
2025-12-26 22:48:53 +0530

    add nic and nic validation

--- Register_voter.c
+++ Register_voter.c
@@ -4,7 +4,17 @@ int voter_Reg() {
     char name[50];
     char pw[12];
     int age;
+    char Nic[12];
     FILE *fp;
+    printf("-----Voter Registration-----\n");
+    printf("Please Enter the following details : \n");
+    printf("Enter Your NIC Number: \n");
+    scanf("%s",Nic);
+    if(strlen(Nic)!=12){
+        printf("Invalid NIC Number\n");
+        return voter_Reg();
+    }
+    
     printf("Enter Your Uername: \n");
     scanf("%s",name);
     printf("Enter password : \n");
@@ -16,7 +26,7 @@ int voter_Reg() {
         if(fp==NULL){
             printf("Can not open file");
         }
-        fprintf(fp,"%s %s\n",name,pw);
+        fprintf(fp,"%s,%s,%s\n",name,pw,Nic);
     }else{
         printf("You are not eligible");
     }
